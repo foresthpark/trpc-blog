8efcb392aa5e3c22581d83d5c82dff7ac4b85b93
Forest Park <foresthpark@gmail.com>
2022-07-18 10:43:14 -0600

    -mui chagnes

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -18,7 +18,7 @@ function VerifyToken({ hash }: { hash: string }) {
     return <p>Verifying...</p>;
   }
 
-  // router.push(data?.redirect.includes("login") ? "/" : data?.redirect || "/");
+  router.push(data?.redirect.includes("login") ? "/" : data?.redirect || "/");
 
   return <p>Redirecting...</p>;
 }
@@ -40,13 +40,61 @@ export default function LoginForm() {
     },
   });
 
-  const onSubmit = async (data: CreateUserInput) => {
+  const submitEmail = async (data: CreateUserInput) => {
+    console.log("🚀 ~ file: LoginForm.tsx ~ line 44 ~ onSubmit ~ data", data);
     mutate({ ...data, redirect: router.asPath });
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="h-screen flex">
+        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div className="mt-8">
+              <div className="mt-6">
+                <form
+                  onSubmit={handleSubmit(submitEmail)}
+                  className="space-y-6"
+                >
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email address
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        typeof="email"
+                        placeholder="example@email.com"
+                        {...register("email")}
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      {isLoading ? "Sending OTP to email..." : "Get OTP"}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:block relative w-0 flex-1">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Login</h1>
         <input
           typeof="email"
@@ -59,7 +107,7 @@ export default function LoginForm() {
         {success && <h1 className="text-orange-600">Check your email</h1>}
         {error && <h1 className="text-red-500">{error.message}</h1>}
       </form>
-      <Link href="/register">Register</Link>
+      <Link href="/register">Register</Link> */}
     </>
   );
 }
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import Link from "next/link";
 import React from "react";
 import { trpc } from "../../utils/trpc";
@@ -11,16 +12,66 @@ const AllPostsPage = () => {
   }
 
   return (
-    <div>
-      {data?.map((post) => {
-        return (
-          <article key={post.id}>
-            <p>{post.title}</p>
-            <Link href={`/posts/${post.id}`}>Read Post</Link>
-          </article>
-        );
-      })}
+    <div className="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+        <div>
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+            The Blog
+          </h2>
+          <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
+            <p className="text-xl text-gray-500">
+              You write stuff. Some are good.
+            </p>
+
+            <div className="mt-2 flex-shrink-0 w-full flex rounded-md shadow-sm sm:mt-0 sm:ml-3 sm:w-auto sm:inline-flex">
+              <Link href={`/posts/new`}>
+                <button
+                  type="button"
+                  className="w-full bg-indigo-600 px-4 py-2 border border-transparent rounded-md flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:inline-flex"
+                >
+                  Create Post
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+          {data.map((post) => (
+            <div key={post.title}>
+              <p className="text-sm text-gray-500">
+                <time dateTime={post.createdAt.toString()}>
+                  {format(post.createdAt, "MMMM d, yyyy")}
+                </time>
+              </p>
+              <a href="#" className="mt-2 block">
+                <p className="text-xl font-semibold text-gray-900">
+                  {post.title}
+                </p>
+                <p className="mt-3 text-base text-gray-500">{post.body}</p>
+              </a>
+              <div className="mt-3">
+                <a
+                  href={`/posts/${post.id}`}
+                  className="text-base font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Read full story
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
+    // <div>
+    //   {data?.map((post) => {
+    //     return (
+    //       <article key={post.id}>
+    //         <p>{post.title}</p>
+    //         <Link href={`/posts/${post.id}`}>Read Post</Link>
+    //       </article>
+    //     );
+    //   })}
+    // </div>
   );
 };
 
